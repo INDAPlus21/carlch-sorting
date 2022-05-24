79dce434b191d52fa5e573c26eaf495e4dee648e
Chemnitz <carlch@kth.se>
2022-05-24 16:39:11 +0200

    Made them working,

---
+++ insert.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#define less(a,b) ((a)<(b))
+#define swap(a,b) do{char t=a; a=b; b=t;}while(0)
+
+void insert_sort(char *l,char *r) {
+  for(char *px=l+1; px<=r; px++)
+    if(less(*px,*l))
+      swap(*px,*l);
+  for(char *px=l+2; px<=r; px++) {
+    char h=*px;
+    char *p=px-1;
+    while(less(h,*p)) {
+      *(p+1)=*p;
+      p-=1;
+    }
+    *(p+1)=h;
+  }
+}
+
+void select_sort(char *a,int len) {
+  register int i,j;
+  for(i=0;i<len;i++)
+    for(j=i+1;j<len;j++)
+      if(*(a+i)>*(a+j))
+        swap(*(a+i),*(a+j));
+
+}
+
+void cocktail_sort(char *a,int len) {
+  do{
+    char flag;
+    int start[2]={1,len-1},
+        end[2]={len,0},
+        inc[2]={1,-1};
+    for(int i=0; i<2; ++i) {
+      flag=1;
+      for(int j=start[i]; j!=end[i]; j+=inc[i])
+        if(a[j-1]>a[j]) {
+          swap(a[j-1],a[j]);
+          flag=0;
+        }
+      if(flag)
+        return;
+    }
+  }while(1);
+}
+
+int main() {
+  FILE *in = fopen("data", "rb");
+  char *vals = (char*)mmap(0, 10*sizeof(int),
+                  PROT_READ, MAP_FILE | MAP_PRIVATE, fileno(in),0);
+  fclose(in);
+  char *a = (char*)malloc(10*sizeof(char));
+  char *b = (char*)malloc(10*sizeof(char));
+  char *c = (char*)malloc(10*sizeof(char));
+  register int i;
+  for(i=0; i<10; i++) {
+    a[i] = vals[i];
+    b[i] = vals[i];
+    c[i] = vals[i];
+    printf("%c ",vals[i]);
+  }
+  printf("\n\n-INSERTION-SORT----\n");
+  insert_sort(a, a+10-1);
+  for(i=0;i<10;i++)
+    printf("%c ",a[i]);
+
+  printf("\n\n-SELECTION-SORT----\n");
+  select_sort(b,10);
+  for(i=0;i<10;i++)
+    printf("%c ",b[i]);
+
+  printf("\n\n-COCKTAIL-SORT-----\n");
+  cocktail_sort(c,10);
+  for(i=0;i<10;i++)
+    printf("%c ",c[i]);
+
+  free(a);
+  free(b);
+  free(c);
+  return 0;
+}
+
